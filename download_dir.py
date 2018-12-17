47270c35e9a4dedb28179484610e062b5b385c5f
Jieren Chen <jieren.chen@gmail.com>
2018-12-17 21:30:33 +0700

    fix

--- download_dir.py
+++ download_dir.py
@@ -13,3 +13,5 @@ def download_dir(file_gen):
         res = requests.get(i[1], stream=True)
         if res.status_code == requests.codes.ok:
           yield PipelineSuccess(name, res.iter_content(chunk_size=1024))
+        else:
+          print(res.status_code)
