3fca59c9fc139be715ebd1e7e47bc8478ba185df
Jieren Chen <jieren.chen@gmail.com>
2018-12-17 21:15:43 +0700

    add more stuff and make some fixes

--- download_dir.py
+++ download_dir.py
@@ -11,4 +11,5 @@ def download_dir(file_gen):
       if str(i[1]).startswith("http"):
         print("Downloading " + i[1])
         res = requests.get(i[1], stream=True)
-        yield PipelineSuccess(name, res.iter_content(chunk_size=1024))
+        if res.status_code == requests.codes.ok:
+          yield PipelineSuccess(name, res.iter_content(chunk_size=1024))
--- train.py
+++ train.py
@@ -1,3 +1,4 @@
+# imported from gukjoon/basic-operations 3ebc4360c6f5ea427582e02e6f1f4d71c49fedbb
 import torch.optim as optim
 import torch.nn as nn
 import torch.onnx
@@ -5,119 +6,57 @@ import torch
 
 from torch.autograd import Variable
 from tensorboardX import SummaryWriter
+from torchvision import transforms
 
-# class ProgressiveLoader:
-#   def __init__(self, image_generator, transforms_in):
-#     self.image_gen = list(image_generator)
-#     self.transform = transforms.Compose(transforms_in)
+class ProgressiveLoader:
+  def __init__(self, image_generator, label_generator, transforms_in):
+    self.image_gen = list(image_generator)
+    self.label_gen = list(label_generator)
+    print('isize', len(self.image_gen))
+    print('lsize', len(self.label_gen))
+    self.transform = transforms.Compose(transforms_in)
 
-#   def __getitem__(self, index):
-#     image = self.image_gen[index].result
-#     print(image)
-#     return self.transform(image)
+  def __getitem__(self, index):
+    image = self.image_gen[index]
+    label = self.label_gen[index]
+    return (self.transform(image), self.transform(label))
 
-#   def name_for(self, index):
-#     return self.image_gen[index].name
-
-#   def __len__(self):
-#     return len(self.image_gen)
-
-
-def train(images_generator, labels_generator, network, loss_function, optimizer_f, epochs):
-  # optimizer, scheduler = optimizer_f(network)
-  # filtered_images = (i for i in images_generator if isinstance(i, PipelineSuccess))  
-  # dataset = ProgressiveLoader(filtered_images, transforms_in)
-  # trainloader = torch.utils.data.DataLoader(
-  #   dataset, 
-  #   batch_size=batch_size
-  #   # num_workers=2
-  # )
-
-  # data_size = len(trainloader)
-  # for epoch in range(epochs):  # loop over the dataset multiple times
-  #   for i, data in enumerate(trainloader, 0):
-  #     iteration = (epoch * data_size) + i
-  #     # get the inputs
-  #     inputs, labels = data
-  #     inputs, labels = inputs.cuda(), labels.cuda()
-  #     # forward + backward + optimize
-  #     outputs = network(inputs)
-  #     loss = criterion(outputs, labels)
-  #     loss.backward()
-  #     # print statistics
-  #     if (iteration % self.nbatch_size) == 0:
-  #       optimizer.step()
-  #       # zero the parameter gradients
-  #       optimizer.zero_grad()
-
-  #   # adjust lr every epoch
-  #   if self.scheduler:
-  #     self.scheduler.step() 
-  yield network
+  def __len__(self):
+    return len(self.image_gen)
 
 
-# class LoggingContext:
-#   def __init__(self, epoch, i, iteration, network, outputs, loss):
-#     self.epoch = epoch
-#     self.i = i
-#     self.iteration = iteration
-#     self.network = network
-#     self.outputs = outputs
-#     self.loss = loss
+def train(images_generator, labels_generator, network, optimizer, loss_function, epochs):
+    network = network.cuda()
+    optimizer, scheduler = optimizer
+    images_generator = list(images_generator)
+    labels_generator = list(labels_generator)
+    file_names = set([f.name for f in images_generator]).intersection(set([f.name for f in labels_generator]))
+    filtered_images = [file_to_image(i.result) for i in images_generator if i.name in file_names]
+    filtered_labels = [file_to_annotation_image(i.result) for i in labels_generator if i.name in file_names]
+    dataset = ProgressiveLoader(filtered_images, filtered_labels, [
+        transforms.Resize((224,224)),
+        transforms.ToTensor()
+    ])
 
-# class Trainer:
-
-#   def __init__(self, dataset, network, criterion, optimizer_f, nbatch_size, loggers):
-#     # loss function
-#     self.criterion = criterion
-#     self.network = network.cuda()
-#     self.optimizer, self.scheduler = optimizer_f(network)
-#     # optim.Adam(network.parameters(), lr=0.00001)
-#     # self.optimizer = optim.SGD(network.parameters(), lr=0.001, momentum=0.9)
-#     self.loader = torch.utils.data.DataLoader(dataset, batch_size=4,
-#                                           shuffle=False, num_workers=2)
-#     self.loggers = loggers
-
-#     writer = SummaryWriter(comment='Net1')
-#     for logger in loggers:
-#       logger.writer = writer
-#       logger.data_size = len(self.loader)
-
-#     self.nbatch_size = nbatch_size
-
-#   def train(self, epochs):
-#     optimizer = self.optimizer
-#     criterion = self.criterion
-#     trainloader = self.loader
-#     network = self.network
-
-#     data_size = len(trainloader)
-#     for epoch in range(epochs):  # loop over the dataset multiple times
-#       for i, data in enumerate(trainloader, 0):
-#         iteration = (epoch * data_size) + i
-#         # get the inputs
-#         inputs, labels = data
-#         inputs, labels = inputs.cuda(), labels.cuda()
-#         # forward + backward + optimize
-#         outputs = network(inputs)
-#         loss = criterion(outputs, labels)
-#         loss.backward()
-#         # print statistics
-#         if (iteration % self.nbatch_size) == 0:
-#           optimizer.step()
-#           # zero the parameter gradients
-#           optimizer.zero_grad()
-
-#         context = LoggingContext(epoch, i, iteration, network, outputs, loss)
-#         for t in self.loggers:
-#           t.run(context)
-
-#       # adjust lr every epoch
-#       if self.scheduler:
-#         self.scheduler.step()
-
-#   def save(self, f):
-#     torch.save(self.network.state_dict(), f)
+    trainloader = torch.utils.data.DataLoader(
+        dataset, 
+        batch_size=8
+        # num_workers=2
+    )
+    
+    data_size = len(trainloader)
+    
+    for epoch in range(epochs):
+        for i, data in enumerate(trainloader, 0):
+            inputs, labels = data
+            inputs, labels = inputs.cuda(), labels.cuda()
+            outputs = network(inputs)
+            loss = loss_function(outputs, labels)
+            loss.backward()
+            optimizer.step()
+            optimizer.zero_grad()
+    print('DONE')
+    return network
 
 #   def save_onnx(self, f):
 #     dummy_input = Variable(torch.randn(4, 3, 224, 224)).cuda()
@@ -125,3 +64,6 @@ def train(images_generator, labels_generator, network, loss_function, optimizer_
 
 #   def save_pth(self, f):
 #     torch.save(self.network, f)
+
+
+
