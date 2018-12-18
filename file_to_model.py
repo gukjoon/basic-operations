1c3269c28c42cd0510a35d36c7443aa8c405ec00
Jieren Chen <jieren.chen@gmail.com>
2018-12-18 20:55:41 +0700

    oops

--- file_to_model.py
+++ file_to_model.py
@@ -2,4 +2,5 @@ import torch
 
 def file_to_model(f, basenetwork):
   state_dict = torch.load(next(f).result)
-  return basenetwork.load_state_dict(state_dict)
+  basenetwork.load_state_dict(state_dict)
+  return basenetwork
