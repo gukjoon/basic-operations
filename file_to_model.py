0e2484a68d8a65d3e509e8d5bc290e1a89bca2c8
Jieren Chen <jieren.chen@gmail.com>
2018-12-18 20:42:12 +0700

    use state dicts

--- file_to_model.py
+++ file_to_model.py
@@ -1,4 +1,5 @@
 import torch
 
-def file_to_model(f):
-  return torch.load(next(f).result)
+def file_to_model(f, basenetwork):
+  state_dict = torch.load(next(f).result)
+  return basenetwork.load_state_dict(state_dict)
