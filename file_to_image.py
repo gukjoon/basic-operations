6872ee1f9839afbd8ce35be63d82d0f69b4ae8c6
Jieren Chen <jieren.chen@gmail.com>
2018-12-18 21:33:26 +0700

    fix

--- file_to_image.py
+++ file_to_image.py
@@ -7,6 +7,6 @@ ImageFile.LOAD_TRUNCATED_IMAGES = True
 def file_to_image(file):
   # Create a copy
   image = Image.open(file)
-  if not image.mode == 'RGB':
+  if not image.mode == 'RGB' and not image.mode == 'RGBA':
     image = ImageMath.eval('im/256', {'im':image}).convert('RGB')  
   return image
