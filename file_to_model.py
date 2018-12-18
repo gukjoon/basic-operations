a539247848e7a73d99ea03d41af18c0e31cecdd3
Jieren Chen <jieren.chen@gmail.com>
2018-12-18 21:02:41 +0700

    try this

--- file_to_model.py
+++ file_to_model.py
@@ -1,6 +1,8 @@
 import torch
 
-def file_to_model(f, basenetwork):
-  state_dict = torch.load(next(f).result)
-  basenetwork.load_state_dict(state_dict)
-  return basenetwork
+def file_to_model(file, network):
+  state_dict = torch.load(next(file).result)
+  print(state_dict)
+  print(network)
+  network.load_state_dict(state_dict)
+  return network
