5c130c2b88be817c74ed06e9cee067d9daccbd80
Jieren Chen <jieren.chen@gmail.com>
2018-12-20 09:57:45 +0700

    some logging

--- unzip.py
+++ unzip.py
@@ -4,4 +4,5 @@ def unzip(file_generator):
   for file in file_generator:
     with zipfile.ZipFile(file.result.name, 'r') as zipped
       for name in zipped.namelist():
+        print(file.name + '/' + name)
         yield PipelineSuccess(file.name + '/' + name, zipped.open(name, 'rb'))
