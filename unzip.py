22fe8e95f2859ee151386603d3e62340c1e7f97e
Jieren Chen <jieren.chen@gmail.com>
2018-12-20 10:23:08 +0700

    fix

--- unzip.py
+++ unzip.py
@@ -2,7 +2,7 @@ import zipfile
 
 def unzip(file_generator):
   for file in file_generator:
-    with zipfile.ZipFile(file.result.name, 'r') as zipped
+    with zipfile.ZipFile(file.result.name, 'r') as zipped:
       for name in zipped.namelist():
         print(file.name + '/' + name)
         yield PipelineSuccess(file.name + '/' + name, zipped.open(name, 'rb'))
