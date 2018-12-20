3cab701427f59a983417f41ab64dc2197a141f04
Jieren Chen <jieren.chen@gmail.com>
2018-12-20 11:58:11 +0700

    fix up select

--- select.py
+++ select.py
@@ -1,5 +1,4 @@
-def select(file, directory):
-  if(file.name.startswith(directory)):
-    return file
-  else
-    return None
+def select(file_generator, str_filter):
+  for file in file_generator:
+    if(file.name.startswith(str_filter)):
+      yield file
