133b0874dfc001f6ed123d8d67b3ed9f5ac492db
Jieren Chen <jieren.chen@gmail.com>
2018-12-24 16:43:09 +0700

    fix?

--- label_map.py
+++ label_map.py
@@ -1,8 +1,8 @@
 def read_label(i):
-    with open(i.result) as f:
-        return f.read().strip()
+  with open(i.result) as f:
+      return f.read().strip()
     
 def label_map(label_gen):
-    label_gen = [read_label(i) for i in label_gen]
-    label_set = sorted(set(label_gen))
-    yield PipelineSuccess('map', {v: idx for idx, v in enumerate(label_set)})
+  label_gen = [read_label(i) for i in label_gen]
+  label_set = sorted(set(label_gen))
+  return {v: idx for idx, v in enumerate(label_set)}
