1e9b6f300ffdb78815ebe39ea4de2d5d6713f2e6
Jieren Chen <jieren.chen@gmail.com>
2018-12-24 16:52:07 +0700

    fix read

--- label_map.py
+++ label_map.py
@@ -1,8 +1,5 @@
-def read_label(i):
-  with open(i.result) as f:
-      return f.read().strip()
-    
+
 def label_map(label_gen):
-  label_gen = [read_label(i) for i in label_gen]
+  label_gen = [i.result.read().strip() for i in label_gen]
   label_set = sorted(set(label_gen))
   return {v: idx for idx, v in enumerate(label_set)}
