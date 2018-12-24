0488b0ba001fc88d3455f9b659b09457e701ed56
Jieren Chen <jieren.chen@gmail.com>
2018-12-24 16:59:24 +0700

    decode

--- label_map.py
+++ label_map.py
@@ -1,5 +1,5 @@
 
 def label_map(label_gen):
-  label_gen = [i.result.read().strip() for i in label_gen]
+  label_gen = [i.result.read().decode('utf-8').strip() for i in label_gen]
   label_set = sorted(set(label_gen))
   return {v: idx for idx, v in enumerate(label_set)}
