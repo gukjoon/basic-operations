10b16b1cd22d854c5c6f2c6136de3e0c26b179c8
Jieren Chen <jieren.chen@gmail.com>
2019-01-06 18:54:37 +0700

    add in yamls

--- file_to_bytes.py
+++
@@ -1,7 +0,0 @@
-import os
-
-def file_to_bytes(file):
-  f = file.name
-  def read_file():
-    return file.read(1024)
-  return iter(read_file, b'')
--- tensor_to_image.py
+++
@@ -1,4 +0,0 @@
-import torchvision.transforms as transforms
-
-def tensor_to_image(tensor):
-  return transforms.ToPILImage()(tensor)
